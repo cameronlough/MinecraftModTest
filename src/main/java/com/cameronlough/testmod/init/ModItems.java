ebd29b132f7d5ef7da036cad0966b5bac4298438
cameronlough <62306894+cameronlough@users.noreply.github.com>
2021-07-15 19:56:03 +1200

    Added item registation and made the modid a constant to be used throughout project

---
+++ src/main/java/com/cameronlough/testmod/init/ModItems.java
@@ -0,0 +1,11 @@
+package com.cameronlough.testmod.init;
+
+import com.cameronlough.testmod.TestMod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.ForgeRegistries;
+
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MODID);
+}
