81ca53505181dcf00a250bf58dee8d6442012263
cameronlough <62306894+cameronlough@users.noreply.github.com>
2021-07-19 22:12:25 +1200

    Added the functionality for the hammer to teleport play 20 block max when right clicked

--- src/main/java/com/cameronlough/testmod/init/ModItems.java
+++ src/main/java/com/cameronlough/testmod/init/ModItems.java
@@ -1,6 +1,7 @@
 package com.cameronlough.testmod.init;
 
 import com.cameronlough.testmod.TestMod;
+import com.cameronlough.testmod.items.Hammer;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.RegistryObject;
@@ -15,7 +16,9 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MODID);
 
     public static RegistryObject<Item> HAMMER = ITEMS.register("hammer",
-            () -> new Item(new Item.Properties().group(ModItemGroup.instance)));
+            () -> new Hammer(new Item.Properties().group(ModItemGroup.instance)));
+
+
 
     public static class ModItemGroup extends ItemGroup {
         public static final ModItemGroup instance = new ModItemGroup(ItemGroup.GROUPS.length,"testmod");
